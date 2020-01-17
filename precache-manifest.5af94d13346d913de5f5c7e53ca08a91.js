f51d4089f52e582b2b678686bd0763ee1dbb9dd7
whydie0 <zzerolink@gmail.com>
2020-01-17 22:14:06 +0300

    deploying site

---
+++ precache-manifest.5af94d13346d913de5f5c7e53ca08a91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdd8bcd90fd8e7adf65da23355ac411",
+    "url": "/algvis-site/index.html"
+  },
+  {
+    "revision": "0a0354be3a8fb07b3827",
+    "url": "/algvis-site/static/css/main.ea15ec10.chunk.css"
+  },
+  {
+    "revision": "1b68d75e6999b9970ff9",
+    "url": "/algvis-site/static/js/2.34962ac0.chunk.js"
+  },
+  {
+    "revision": "3e54b583583f001f171d219930dc6b65",
+    "url": "/algvis-site/static/js/2.34962ac0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a0354be3a8fb07b3827",
+    "url": "/algvis-site/static/js/main.541553bc.chunk.js"
+  },
+  {
+    "revision": "449e81add1f5607f9f39",
+    "url": "/algvis-site/static/js/runtime-main.4cf7d802.js"
+  },
+  {
+    "revision": "5f0eefe1d0257a111013c232ac626562",
+    "url": "/algvis-site/static/media/PFDinMono-Light.5f0eefe1.ttf"
+  },
+  {
+    "revision": "916365b565d5448e6211c2382a2c8f70",
+    "url": "/algvis-site/static/media/PFDinMono-Regular.916365b5.ttf"
+  }
+]);
